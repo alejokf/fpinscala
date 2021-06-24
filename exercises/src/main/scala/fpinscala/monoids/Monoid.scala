4d25a01ce9cd72933913cd02681ce43c55d024c0
Alejandro Marulanda <alejokf@gmail.com>
2021-06-24 13:50:43 +0200

    Chapter 10 exercises - incomplete

--- exercises/src/main/scala/fpinscala/monoids/Monoid.scala
+++ exercises/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -21,17 +21,49 @@ object Monoid {
     val zero = Nil
   }
 
-  val intAddition: Monoid[Int] = ???
+  val intAddition: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 + a2
 
-  val intMultiplication: Monoid[Int] = ???
+    override val zero: Int = 0
+  }
 
-  val booleanOr: Monoid[Boolean] = ???
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 * a2
 
-  val booleanAnd: Monoid[Boolean] = ???
+    override val zero: Int = 0
+  }
 
-  def optionMonoid[A]: Monoid[Option[A]] = ???
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
 
-  def endoMonoid[A]: Monoid[A => A] = ???
+    override val zero: Boolean = false
+  }
+
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+
+    override val zero: Boolean = true
+  }
+
+  // Not sure is implementation of op is associative
+  def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    override def op(a1: Option[A], a2: Option[A]): Option[A] = a1.orElse(a2)
+
+    override def zero: Option[A] = None
+  }
+
+  // Not sure is implementation of op is associative
+  def endoMonoid[A]: Monoid[A => A] = new Monoid[A => A] {
+    override def op(a1: A => A, a2: A => A): A => A = a1.compose(a2)
+
+    override def zero: A => A = (a: A) => a
+  }
+
+  // dual is the monoid having op wit arguments flipped
+  def dual[A](m: Monoid[A]): Monoid[A] = new Monoid[A] {
+    def op(x: A, y: A): A = m.op(y, x)
+    val zero = m.zero
+  }
 
   // TODO: Placeholder for `Prop`. Remove once you have implemented the `Prop`
   // data type from Part 2.
@@ -46,27 +78,37 @@ object Monoid {
 
   def trimMonoid(s: String): Monoid[String] = ???
 
-  def concatenate[A](as: List[A], m: Monoid[A]): A =
-    ???
+  def concatenate[A](as: List[A], m: Monoid[A]): A = {
+    //from book
+    as.foldLeft(m.zero)(m.op)
+  }
 
-  def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B =
-    ???
+  def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B = {
+    //option 1
+    //concatenate(as.map(f), m)
+    //option 2
+    as.foldLeft(m.zero)((b: B, a: A) => m.op(b, f(a)))
+  }
 
   def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B =
-    ???
+    foldMap(as, endoMonoid[B])(f.curried)(z)
 
   def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B =
-    ???
+    foldMap(as, dual(endoMonoid[B]))(a => (b => f(b, a)))(z)
 
+  // foldMap for IndexedSeq
   def foldMapV[A, B](as: IndexedSeq[A], m: Monoid[B])(f: A => B): B =
-    ???
+    if (as.isEmpty)
+      m.zero
+    else if (as.length == 1)
+      f(as(0))
+    else {
+      val (left, right) = as.splitAt(as.length / 2)
+      m.op(foldMapV(left, m)(f), foldMapV(right, m)(f))
+    }
 
   def ordered(ints: IndexedSeq[Int]): Boolean =
     ???
-
-  sealed trait WC
-  case class Stub(chars: String) extends WC
-  case class Part(lStub: String, words: Int, rStub: String) extends WC
 
   def par[A](m: Monoid[A]): Monoid[Par[A]] = 
     ???
@@ -74,12 +116,44 @@ object Monoid {
   def parFoldMap[A,B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): Par[B] = 
     ???
 
-  val wcMonoid: Monoid[WC] = ???
+  sealed trait WC
+  case class Stub(chars: String) extends WC
+  case class Part(lStub: String, words: Int, rStub: String) extends WC
 
-  def count(s: String): Int = ???
+  val wcMonoid: Monoid[WC] = new Monoid[WC] {
+    override def op(a1: WC, a2: WC): WC = (a1, a2) match {
+      // to combine 2 Stubs we simply concatenate both chars in another Stub
+      case (Stub(c1), Stub(c2)) => Stub(c1 + c2)
+      // to combine 1 Stub and 1 Part we concatenate the chars of the Stub to the left or right Stub of the Part
+      case (Stub(c), Part(l, w, r)) => Part(c + l, w, r)
+      case (Part(l, w, r), Stub(c)) => Part(l, w, r + c)
+      // to combine two parts, we check if the left Stub of part1 and right Stub of part2 form a word, and add 1 to the word count
+      case (Part(l1, w1, r1), Part(l2, w2, r2)) => Part(l1, w1 + w2 + (if ((l2 + r1).isEmpty) 0 else 1), r2)
+    }
 
-  def productMonoid[A,B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] =
-    ???
+    override def zero: WC = Stub("")
+  }
+
+  def count(s: String): Int = {
+    def toWC(c: Char): WC =
+      if (c.isWhitespace)
+        Part("", 0, "")
+      else
+        Stub(c.toString)
+
+    def countWord(s: String) = s.length.min(1)
+
+    foldMapV(s.toIndexedSeq, wcMonoid)(toWC) match {
+      case Stub(c) => countWord(c)
+      case Part(l, w, r) => countWord(l) + w + countWord(r)
+    }
+  }
+
+  def productMonoid[A,B](a: Monoid[A], b: Monoid[B]): Monoid[(A, B)] = new Monoid[(A, B)] {
+    override def op(a1: (A, B), a2: (A, B)): (A, B) = (a.op(a1._1, a1._1), b.op(a1._2, a2._2))
+
+    override def zero: (A, B) = (a.zero, b.zero)
+  }
 
   def functionMonoid[A,B](B: Monoid[B]): Monoid[A => B] =
     ???
